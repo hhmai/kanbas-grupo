1ff29171ceee912a29f6231977e4fbf76949ce81
aldawgy <110347220+aldawgy@users.noreply.github.com>
2024-02-23 23:42:16 -0500

    lab3 done

---
+++ src/Labs/a3/JavaScript/index.tsx
@@ -0,0 +1,56 @@
+import AddingAndRemovingDataTofromArrays from "./arrays/AddingAndRemovingDataToFromArrays";
+import ArrayIndexAndLength from "./arrays/ArrayIndexAndLength";
+import FilterFunction from "./arrays/FilterFunction";
+import FindFunction from "./arrays/FindFunction";
+import FindIndex from "./arrays/FindIndex";
+import ForLoops from "./arrays/ForLoops";
+import MapFunction from "./arrays/MapFunction";
+import WorkingWithArrays from "./arrays/WorkingWithArrays";
+import IfElse from "./conditionals/IfElse";
+import TernaryOperator from "./conditionals/TernaryOperator";
+import ArrowFunctions from "./functions/ArrowFunctions";
+import ES5Functions from "./functions/ES5Functions";
+import FunctionDestructing from "./functions/FunctionDestructuring";
+import FunctionParenthesisAndParameters from "./functions/FunctionParenthesisAndParameters";
+import ImpliedReturn from "./functions/ImpliedReturn";
+import Destructing from "./json/Destructuring";
+import House from "./json/House";
+import JsonStringify from "./json/JsonStringify";
+import Spreading from "./json/Spreading";
+import TemplateLiterals from "./string/TemplateLiterals";
+import BooleanVariables from "./variables/BooleanVariables";
+import VariableTypes from "./variables/VariableTypes";
+import VariablesAndConstants from "./variables/VariablesAndConstants";
+
+function JavaScript() {
+    console.log('Hello World!');
+    return(
+       <div>
+        <h1>JavaScript</h1>
+         <VariablesAndConstants/>
+         <VariableTypes/>
+         <BooleanVariables/>
+         <IfElse/>
+         <TernaryOperator/>
+         <ES5Functions/>
+         <ArrowFunctions/>
+         <ImpliedReturn/>
+         <FunctionParenthesisAndParameters/>
+         <WorkingWithArrays/>
+         <ArrayIndexAndLength/>
+         <AddingAndRemovingDataTofromArrays/>
+         <ForLoops/>
+         <MapFunction/>
+         <JsonStringify/>
+         <FindFunction/>
+         <FindIndex/>
+         <FilterFunction/>
+         <TemplateLiterals/>
+         <House/>
+         <Spreading/>
+         <Destructing/>
+         <FunctionDestructing/>
+       </div>
+    );
+ }
+ export default JavaScript
