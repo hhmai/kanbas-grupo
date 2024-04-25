51b0fbb3731f989e67cfbce151c47837c98210c5
horacemai <horacehymai@gmail.com>
2024-04-25 12:09:56 -0400

    IMDB api fixes

--- src/OMDB/client.js
+++ src/OMDB/client.js
@@ -2,7 +2,7 @@ const apiKey = '65411a9d';
 
 export const getMovieByTitle = async (title) => {
     try {
-      const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(title)}`);
+      const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(title)}`);
       if (!response.ok) {
         throw new Error('Failed to fetch movie data');
       }
@@ -16,7 +16,7 @@ export const getMovieByTitle = async (title) => {
 
 export const getMovieByTitleYear = async (title, year) => {
     try {
-      const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(title)}&y=${year}`);
+      const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(title)}&y=${year}`);
       if (!response.ok) {
         throw new Error('Failed to fetch movie data');
       }
